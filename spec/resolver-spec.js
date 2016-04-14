1a68ecb22d9203678d3d4c4b6e49e07f1c85f16e
steelbrain <de.steelbrain@gmail.com>
2016-04-14 09:23:57 -0700

    :arrow_up: Upgrade specs to match latest internal API

--- spec/resolver-spec.js
+++ spec/resolver-spec.js
@@ -8,10 +8,10 @@ import * as Resolver from '../lib/resolver'
 describe('Resolver', function() {
   const defaultConfig = Helpers.fillConfig({})
 
-  describe('resolveOnFileSystem', function() {
+  describe('resolve', function() {
     it('rejects with a proper error', async function() {
       try {
-        await Resolver.resolveOnFileSystem('test', __dirname, defaultConfig)
+        await Resolver.resolve(defaultConfig, 'test', 'test', __dirname)
         expect(false).toBe(true)
       } catch (_) {
         expect(_.code).toBe('MODULE_NOT_FOUND')
@@ -19,23 +19,27 @@ describe('Resolver', function() {
       }
     })
     it('resolves direct files properly', async function() {
-      expect(await Resolver.resolveOnFileSystem('x', __filename, defaultConfig)).toBe(__filename)
+      expect(await Resolver.resolve(defaultConfig, 'x', __filename)).toBe(__filename)
     })
     it('resolves modules with manifests properly', async function() {
       const mainFile = Path.normalize(Path.join(__dirname, '..', 'lib', 'index.js'))
-      expect(await Resolver.resolveOnFileSystem('x', Path.dirname(__dirname), defaultConfig)).toBe(mainFile)
+      expect(await Resolver.resolve(defaultConfig, 'x', Path.dirname(__dirname))).toBe(mainFile)
     })
     it('resolves modules with . or ./ in main properely', async function() {
       const specDir = Path.join(__dirname, 'fixtures', 'dot-in-main')
-      expect(await Resolver.resolveOnFileSystem('x', specDir, defaultConfig)).toBe(
+      expect(await Resolver.resolve(defaultConfig, 'x', specDir)).toBe(
         Path.join(specDir, 'index.js')
       )
     })
     it('resolves modules without manifests', async function() {
       const specDir = Path.join(__dirname, 'fixtures', 'no-manifest')
-      expect(await Resolver.resolveOnFileSystem('x', specDir, defaultConfig)).toBe(
+      expect(await Resolver.resolve(defaultConfig, 'x', specDir)).toBe(
         Path.join(specDir, 'index.json')
       )
     })
+    it('resolves local references properly', async function() {
+      expect(await Resolver.resolve(defaultConfig, './helpers', Path.join(__dirname, './helpers'))).toBe(require.resolve('./helpers'))
+    })
+    // TODO: A module with a manifest but no main
   })
 })
